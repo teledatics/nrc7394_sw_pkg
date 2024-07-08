2be70ff5508e272c4386954349c79339ed949162
root <james@teledatics.com>
2024-07-08 10:02:08 -0400

    Prefer dynamic SPI over DT

--- package/src/nrc/nrc-build-config.h
+++ package/src/nrc/nrc-build-config.h
@@ -215,7 +215,7 @@
 #endif
 
 /* Prefer dynamic spi over device tree */
-#if !defined(CONFIG_SPI_USE_DT) && KERNEL_VERSION(5, 16, 0) <= NRC_TARGET_KERNEL_VERSION
+#if defined(CONFIG_SPI_USE_DT) && KERNEL_VERSION(5, 16, 0) <= NRC_TARGET_KERNEL_VERSION
 #define CONFIG_SPI_DYNAMIC
 #undef CONFIG_SPI_USE_DT
 #endif
