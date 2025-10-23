dea1143f950a517eeb09f36718793f9f8930c0b7
Dan Theisen <djt@hxx.in>
2025-10-22 21:13:04 -0700

    Support Linux 6.12

--- package/src/nrc/nrc-trace.h
+++ package/src/nrc/nrc-trace.h
@@ -114,8 +114,8 @@ DECLARE_EVENT_CLASS(nrc_hdr_event,
 		struct ieee80211_hdr *hdr = (void *)data;
 		__le16 fc = hdr->frame_control;
 
-		__assign_str(device, dev_name(nw->dev));
-		__assign_str(driver, dev_driver_string(nw->dev));
+		__assign_str(device);
+		__assign_str(driver);
 		__entry->fc_type = (fc & cpu_to_le16(IEEE80211_FCTL_FTYPE));
 		__entry->fc_subtype = (fc & cpu_to_le16(IEEE80211_FCTL_STYPE));
 		__entry->fc_etc = fc & cpu_to_le16(0xFF00);
@@ -190,8 +190,8 @@ DECLARE_EVENT_CLASS(nrc_payload_event,
 	),
 
 	TP_fast_assign(
-		__assign_str(device, dev_name(nw->dev));
-		__assign_str(driver, dev_driver_string(nw->dev));
+		__assign_str(device);
+		__assign_str(driver);
 		__entry->len = len - nrc_frm_hdr_len(data, len);
 		memcpy(__get_dynamic_array(payload),
 		       data + nrc_frm_hdr_len(data, len), min_t(size_t, __entry->len, MAX_PAYLOAD));
@@ -248,7 +248,7 @@ DECLARE_EVENT_CLASS(nrc_hif_slot_event,
 	),
 
 	TP_fast_assign(
-		__assign_str(msg, msg);
+		__assign_str(msg);
 		__entry->dir = dir;
 		__entry->head = priv->slot[dir].head;
 		__entry->tail = priv->slot[dir].tail;
